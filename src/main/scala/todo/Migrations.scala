b03fb2451f91e82d6d90547c14efa421e60eaf70
Tom Poczos <t.poczos@gmail.com>
2020-02-09 19:18:08 +0100

    migrations

--- src/main/scala/todo/Migrations.scala
+++ src/main/scala/todo/Migrations.scala
@@ -10,16 +10,28 @@ object Migrations {
   val migrations =
     List(
       sql"""
-         |create table if not exists todo(
-         | id integer primary key,
-         | name text not null,
-         | done tinyint not null default 0
-         |)
-         |"""
+           |PRAGMA foreign_keys = ON
+           |""",
+      sql"""
+           |create table if not exists user(
+           |  id integer primary key,
+           |  name text,
+           |  salt text,
+           |  pwdHash text not null
+           |)
+           |""",
+      sql"""
+           |create table if not exists todo(
+           |  id integer primary key,
+           |  user_fk integer references user(id),
+           |  name text not null,
+           |  done tinyint not null default 0
+           |)
+           |""",
     )
 
   def run(transactor: Transactor[IO]): IO[List[Int]] =
-      migrations
-        .map(_.stripMargin.update.run.transact(transactor))
-        .sequence
+    migrations.traverse {
+      _.stripMargin.update.run.transact(transactor)
+    }
 }
