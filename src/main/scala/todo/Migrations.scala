ab7172fd0d1855a004d061ea92a6c220c7eabc7e
Tom Poczos <t.poczos@gmail.com>
2020-02-09 14:40:42 +0100

    multiple migrations

--- src/main/scala/todo/Migrations.scala
+++ src/main/scala/todo/Migrations.scala
@@ -1,24 +1,25 @@
 package todo
 
 import cats.effect.IO
-import cats.syntax.functor._
+import cats.implicits._
 import doobie.implicits._
 import doobie.Transactor
 
 object Migrations {
 
-  val migrationV1 = {
-    sql"""
+  val migrations =
+    List(
+      sql"""
          |create table if not exists todo(
          | id integer primary key,
          | name text not null,
          | done tinyint not null default 0
          |)
-         |""".stripMargin
-  }
+         |"""
+    )
 
-  def run(transactor: Transactor[IO]): IO[Unit] = {
-    migrationV1.update.run.transact(transactor).void
-  }
-
+  def run(transactor: Transactor[IO]): IO[List[Int]] =
+      migrations
+        .map(_.stripMargin.update.run.transact(transactor))
+        .sequence
 }
