f927becf0546f80966535faeeb6e9f842c858f52
Tom Poczos <t.poczos@gmail.com>
2020-02-09 14:56:41 +0100

    decouple routes from IO

--- src/main/scala/todo/Routes.scala
+++ src/main/scala/todo/Routes.scala
@@ -1,17 +1,18 @@
 package todo
 
 import Model.{CreateTodo, EmptyResponse, ErrorResponse, Login}
-import cats.effect.IO
+import cats.Monad
+import cats.effect.{ConcurrentEffect, IO, Sync}
 import org.http4s.circe.{CirceEntityEncoder, CirceInstances}
 import org.http4s.rho.RhoRoutes
 import org.http4s.rho.swagger.SwaggerSyntax
 import todo.dataaccess.Algebras.TodoDao
 import cats.implicits._
 
-class Routes(dao: TodoDao[IO, List]) {
+class Routes[F[+_]:ConcurrentEffect](dao: TodoDao[F, List]) {
 
-  val todoRoutes: RhoRoutes[IO] =
-    new RhoRoutes[IO] with SwaggerSyntax[IO] with CirceInstances with CirceEntityEncoder {
+  val todoRoutes: RhoRoutes[F] =
+    new RhoRoutes[F] with SwaggerSyntax[F] with CirceInstances with CirceEntityEncoder {
       // ----------------------------------------------------------------------------------------------------------------------- //
       //  NOTE: If you run into issues with divergent implicits check out this issue https://github.com/http4s/rho/issues/292   //
       // ---------------------------------------------------------------------------------------------------------------------- //
@@ -22,7 +23,7 @@ class Routes(dao: TodoDao[IO, List]) {
           .flatMap(Ok(_))
       }
 
-      POST / "todo" ^ jsonOf[IO, CreateTodo] |>> { createTodo: CreateTodo =>
+      POST / "todo" ^ jsonOf[F, CreateTodo] |>> { createTodo: CreateTodo =>
         dao
           .create(createTodo.name)
           .flatMap(_ => Ok(EmptyResponse()))
@@ -32,30 +33,30 @@ class Routes(dao: TodoDao[IO, List]) {
         dao.markAsDone(todoId).flatMap {
           case 0 => NotFound(ErrorResponse(s"Todo with id: `${todoId}` not found"))
           case 1 => Ok(EmptyResponse())
-          case _ =>
-            IO(println(s"Inconsistent data: More than one todo updated in POST /todo/${todoId}")) *>
-              InternalServerError(ErrorResponse("Ooops, something went wrong..."))
+//          case _ =>
+//            IO(println(s"Inconsistent data: More than one todo updated in POST /todo/${todoId}")) *>
+//              InternalServerError(ErrorResponse("Ooops, something went wrong..."))
         }
       }
     }
 
-  val userRoutes: RhoRoutes[IO] =
-    new RhoRoutes[IO] with SwaggerSyntax[IO] with CirceInstances with CirceEntityEncoder {
+  val userRoutes: RhoRoutes[F] =
+    new RhoRoutes[F] with SwaggerSyntax[F] with CirceInstances with CirceEntityEncoder {
 
       "Login" **
-        POST / "auth" ^ jsonOf[IO, Login] |>> { login: Login =>
+        POST / "auth" ^ jsonOf[F, Login] |>> { login: Login =>
         login.username
       }
 
       "Create a new user" **
-        POST / "auth" / "new" ^ jsonOf[IO, Login] |>> { login: Login =>
+        POST / "auth" / "new" ^ jsonOf[F, Login] |>> { login: Login =>
         //        login.username
       }
     }
 }
 
 object Routes {
-  def apply(dao: TodoDao[IO, List]) = {
+  def apply[F[+_]:ConcurrentEffect:Sync:Monad](dao: TodoDao[F, List]) = {
     val routes = new Routes(dao)
     routes.todoRoutes and routes.userRoutes
   }
